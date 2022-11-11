b470d7396c839d4e236c11afe6eefc228e6b4b40
roxas <jeropastrana@gmail.com>
2022-11-11 16:26:08 +0100

    Initial commit

---
+++ src/com/cieep/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String frase = sc.nextLine();
+
+        frase = frase.toLowerCase().replace('e', 'a');
+        frase = frase.toLowerCase().replace('E', 'A');
+
+        StringTokenizer str = new StringTokenizer(frase);
+
+        while (str.hasMoreTokens()) {
+            System.out.println(str.nextToken());
+        }
+        System.out.println("FIN");
+    }
+}
